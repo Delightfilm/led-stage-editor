493a7b23f11db03ad9a017c3c5e0d9e533f3d9f4
Delightfilm <jhpodong000000@gmail.com>
2026-08-11 13:07:25 +0900

    feat: enable packed nRF24 timeline generation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nrf24LiveCodePlugin } from './scripts/nrf24LiveCodePlugin.js'
+import { nrf24CompressedLiveCodePlugin } from './scripts/nrf24CompressedLiveCodePlugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [nrf24LiveCodePlugin(), react()],
+  plugins: [nrf24CompressedLiveCodePlugin(), react()],
 })
