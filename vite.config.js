3d1cb357127baa411ca15811e0088ca3126ae03c
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 17:07:15 +0900

    Switch to stable Premiere video editor transform

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import { cloudAudioStoragePlugin } from './scripts/cloudAudioStoragePlugin.js'
 import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
 import { defaultRelayPinPlugin } from './scripts/defaultRelayPinPlugin.js'
-import { premiereVideoEditorPlugin } from './scripts/premiereVideoEditorPlugin.js'
+import { premiereVideoEditorPluginV2 } from './scripts/premiereVideoEditorPluginV2.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), premiereVideoEditorPlugin(), react()],
+  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), premiereVideoEditorPluginV2(), react()],
 })
