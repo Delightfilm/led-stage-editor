a7c59a1100bdd4863c0d3f701b12dbc9cd9814c6
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 15:52:17 +0900

    Wire startup tutorial disable plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { cloudAudioStoragePlugin } from './scripts/cloudAudioStoragePlugin.js'
+import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [cloudAudioStoragePlugin(), react()],
+  plugins: [disableTutorialPlugin(), cloudAudioStoragePlugin(), react()],
 })
