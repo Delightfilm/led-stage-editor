560b3d77dd8183ba5ec6fd83ce6460f33e510713
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 17:03:57 +0900

    Enable Premiere-style video editing UI

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import { cloudAudioStoragePlugin } from './scripts/cloudAudioStoragePlugin.js'
 import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
 import { defaultRelayPinPlugin } from './scripts/defaultRelayPinPlugin.js'
+import { premiereVideoEditorPlugin } from './scripts/premiereVideoEditorPlugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), react()],
+  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), premiereVideoEditorPlugin(), react()],
 })
