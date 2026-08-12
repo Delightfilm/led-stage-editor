4f15217cc821131a7767e993034521b23df3eb62
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 17:23:45 +0900

    Guard Premiere video transform in Vercel builds

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import { cloudAudioStoragePlugin } from './scripts/cloudAudioStoragePlugin.js'
 import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
 import { defaultRelayPinPlugin } from './scripts/defaultRelayPinPlugin.js'
 import { premiereVideoEditorPluginV2 } from './scripts/premiereVideoEditorPluginV2.js'
+import { premiereVideoBuildGuardPlugin } from './scripts/premiereVideoBuildGuardPlugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), premiereVideoEditorPluginV2(), react()],
+  plugins: [
+    defaultRelayPinPlugin(),
+    disableTutorialPlugin(),
+    cloudAudioStoragePlugin(),
+    premiereVideoEditorPluginV2(),
+    premiereVideoBuildGuardPlugin(),
+    react(),
+  ],
 })
