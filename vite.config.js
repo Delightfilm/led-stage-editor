bf2cec2dda525d599e1d8d9e011cb9119f730f9c
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 17:46:00 +0900

    Enable frame zoom, clip snapping, and cloud video sync

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
 import { defaultRelayPinPlugin } from './scripts/defaultRelayPinPlugin.js'
 import { premiereVideoEditorPrePlugin } from './scripts/premiereVideoEditorPrePlugin.js'
 import { premiereTimelinePolishPlugin } from './scripts/premiereTimelinePolishPlugin.js'
+import { premiereTimelineProPlugin } from './scripts/premiereTimelineProPlugin.js'
+import { cloudMediaStoragePlugin } from './scripts/cloudMediaStoragePlugin.js'
 import { premiereVideoBuildGuardPlugin } from './scripts/premiereVideoBuildGuardPlugin.js'
 
 // https://vite.dev/config/
@@ -15,6 +17,8 @@ export default defineConfig({
     cloudAudioStoragePlugin(),
     premiereVideoEditorPrePlugin(),
     premiereTimelinePolishPlugin(),
+    premiereTimelineProPlugin(),
+    cloudMediaStoragePlugin(),
     premiereVideoBuildGuardPlugin(),
     react(),
   ],
