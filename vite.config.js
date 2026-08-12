e2f3e63e305247e29cdce2a34b4e1259bdd9cfbc
Delightfilm <jhpodong000000@gmail.com>
2026-08-12 15:57:09 +0900

    Apply D4 as default RX relay pin

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { cloudAudioStoragePlugin } from './scripts/cloudAudioStoragePlugin.js'
 import { disableTutorialPlugin } from './scripts/disableTutorialPlugin.js'
+import { defaultRelayPinPlugin } from './scripts/defaultRelayPinPlugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [disableTutorialPlugin(), cloudAudioStoragePlugin(), react()],
+  plugins: [defaultRelayPinPlugin(), disableTutorialPlugin(), cloudAudioStoragePlugin(), react()],
 })
