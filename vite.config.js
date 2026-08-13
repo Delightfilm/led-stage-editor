23cc42d8209a282e617ec6c4b3edc56263dc4aa4
Delightfilm <jhpodong000000@gmail.com>
2026-08-14 00:38:54 +0900

    Enable explicit A and B stage mode controls

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { managementSerialSafetyPlugin } from './scripts/managementSerialSafetyPlugin.js'
 import { managementFirmwarePanelPlugin } from './scripts/managementFirmwarePanelPlugin.js'
+import { managementABModePlugin } from './scripts/managementABModePlugin.js'
 
 export default defineConfig({
-  plugins: [managementFirmwarePanelPlugin(), managementSerialSafetyPlugin(), react()],
+  plugins: [managementABModePlugin(), managementFirmwarePanelPlugin(), managementSerialSafetyPlugin(), react()],
 })
