3ddb7ee778980200daf4a6728388335f8eb01dbf
Delightfilm <jhpodong000000@gmail.com>
2026-08-14 01:15:33 +0900

    Enable live RF monitor plugin

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import { managementSerialSafetyPlugin } from './scripts/managementSerialSafetyPlugin.js'
 import { managementFirmwarePanelPlugin } from './scripts/managementFirmwarePanelPlugin.js'
 import { managementABModePlugin } from './scripts/managementABModePlugin.js'
+import { liveMonitorPlugin } from './scripts/liveMonitorPlugin.js'
 
 export default defineConfig({
   // Firmware bundle must be injected before the A/B mode helpers because
   // the A/B controls read firmwareBundle during render.
-  plugins: [managementFirmwarePanelPlugin(), managementABModePlugin(), managementSerialSafetyPlugin(), react()],
+  plugins: [managementFirmwarePanelPlugin(), managementABModePlugin(), liveMonitorPlugin(), managementSerialSafetyPlugin(), react()],
 })
