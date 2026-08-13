e3f0d36d86359b33e9f7f4f19fe1f3656a71005b
Delightfilm <jhpodong000000@gmail.com>
2026-08-14 00:36:19 +0900

    Enable management A/B firmware export panel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { managementSerialSafetyPlugin } from './scripts/managementSerialSafetyPlugin.js'
+import { managementFirmwarePanelPlugin } from './scripts/managementFirmwarePanelPlugin.js'
 
 export default defineConfig({
-  plugins: [managementSerialSafetyPlugin(), react()],
+  plugins: [managementFirmwarePanelPlugin(), managementSerialSafetyPlugin(), react()],
 })
