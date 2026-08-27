72172ff05c96bac87cae80640c4b51f5d350792f
Delightfilm <jhpodong000000@gmail.com>
2026-08-28 02:11:39 +0900

    Enable direct AudioWorklet microphone capture

--- vite.sync-live-mic.config.js
+++ vite.sync-live-mic.config.js
@@ -1,10 +1,12 @@
 import baseConfig from './vite.config.js'
 import { syncLiveMicInputFixPlugin } from './scripts/syncLiveMicInputFixPlugin.js'
+import { syncLiveAudioWorkletFixPlugin } from './scripts/syncLiveAudioWorkletFixPlugin.js'
 
 export default {
   ...baseConfig,
   plugins: [
     syncLiveMicInputFixPlugin(),
+    syncLiveAudioWorkletFixPlugin(),
     ...(baseConfig.plugins || []),
   ],
 }
