da9df30a30b51c67a50e15edc45e2abe97b789d0
Delightfilm <jhpodong000000@gmail.com>
2026-08-28 03:12:35 +0900

    Enable Sync Live V5 time lock build

--- vite.sync-live-mic.config.js
+++ vite.sync-live-mic.config.js
@@ -1,12 +1,14 @@
 import baseConfig from './vite.config.js'
 import { syncLiveMicInputFixPlugin } from './scripts/syncLiveMicInputFixPlugin.js'
 import { syncLiveAudioWorkletFixPlugin } from './scripts/syncLiveAudioWorkletFixPlugin.js'
+import { syncLiveTimeLockV5Plugin } from './scripts/syncLiveTimeLockV5Plugin.js'
 
 export default {
   ...baseConfig,
   plugins: [
     syncLiveMicInputFixPlugin(),
     syncLiveAudioWorkletFixPlugin(),
+    syncLiveTimeLockV5Plugin(),
     ...(baseConfig.plugins || []),
   ],
 }
